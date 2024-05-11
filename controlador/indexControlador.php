2389b7a3f97ea29a8548aabd60a8cfa19a2ab66c
Marquito Almada <marcosalmada1067@gmail.com>
2024-05-10 22:57:58 -0300

    cambio de nombres y conexion exitosa con la base en grupos

--- controlador/indexControlador.php
+++ controlador/indexControlador.php
@@ -1,2 +1,21 @@
 <?php
+require_once "../bd/conn.php";
+require_once "../modelo/grupo.php";
 
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+
+class IndexControlador extends Grupo
+{
+
+    public function vista()
+    {
+        $listadoGrupos = $this->buscarGrupos();
+        require "../vista/index.php";
+    }
+}
+
+$ii = new IndexControlador();
+$ii->vista();
--- vista/index.php
+++ vista/index.php
@@ -1,126 +1,57 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/index.css" />
-    <title>Albumes Spineta</title>
-  </head>
-  <body>
-    <div class="div-titulo">
-      <h1>Discografía de Spinetta</h1>
-      <span class="frase"
-        >"Tengo que aprender a volar entre tanta gente de pie"</span
-      >
-      <span>-</span>
-      <span>L.A Spinetta</span>
-    </div>
 
-    <div class="container-div-grupos">
-      <div class="div-grupos">
-        <h2>En Solitario</h2>
-        <div class="element-grupos solitario">
-          <div class="img-container">
-            <img class="img-grupo" src="../images/grupos/Solitario.jpg" />
-          </div>
-          <div class="div-descripcion">
-            <span class="nombre">Solitario</span>
-            <p class="descripcion">Luis Alberto Spinetta, que crac.</p>
-          </div>
-        </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../css/index.css" />
+  <title>Albumes Spineta</title>
+</head>
 
-        <h2>Grupos Musicales</h2>
-        <div class="grupos">
-          <div class="element-grupos">
+<body>
+  <div class="div-titulo">
+    <h1>Discografía de Spinetta</h1>
+    <span class="frase">"Tengo que aprender a volar entre tanta gente de pie"</span>
+    <span>-</span>
+    <span>L.A Spinetta</span>
+  </div>
+
+  <div class="container-div-grupos">
+    <div class="div-grupos">
+      <h2>En Solitario</h2>
+
+      <?php foreach ($listadoGrupos as $grupo) {
+        if ($grupo->nombre_controlador == "solitario") { ?>
+          <div class="element-grupos solitario">
             <div class="img-container">
-              <img class="img-grupo" src="../images/grupos/Almendra.jpg" />
+              <img class="img-grupo" src="../images/grupos/<?php echo $grupo->nombre_controlador; ?>.jpg" />
             </div>
             <div class="div-descripcion">
-              <span class="nombre">Almendra</span>
-              <p class="descripcion">
-                Almendra, formada en 1967 en Buenos Aires por Luis Alberto
-                Spinetta, Edelmiro Molinari, Emilio del Guercio y Rodolfo
-                García.
-              </p>
+              <span class="nombre"><?php echo $grupo->nombre_vista; ?></span>
+              <p class="descripcion"><?php echo $grupo->descripcion; ?></p>
             </div>
           </div>
+        <?php }
+      } ?>
 
-          <div class="element-grupos">
-            <div class="img-container">
-              <img
-                class="img-grupo"
-                src="../images/grupos/Pescado-Rabioso.jpg"
-              />
+      <h2>Grupos Musicales</h2>
+      <div class="grupos">
+        <?php foreach ($listadoGrupos as $grupo) {
+          if ($grupo->nombre_controlador != "solitario") { ?>
+            <div class="element-grupos">
+              <div class="img-container">
+                <img class="img-grupo" src="../images/grupos/<?php echo $grupo->nombre_controlador; ?>.jpg" />
+              </div>
+              <div class="div-descripcion">
+                <span class="nombre"><?php echo $grupo->nombre_vista; ?></span>
+                <p class="descripcion"><?php echo $grupo->descripcion; ?></p>
+              </div>
             </div>
-            <div class="div-descripcion">
-              <span class="nombre">Pescado Rabioso</span>
-              <p class="descripcion">
-                Formado en 1971 por Luis Alberto Spinetta, David Lebón, Black
-                Amaya y Carlos Cutaia. Introdujo el hard rock, blues y rock
-                psicodélico a la escena nacional.
-              </p>
-            </div>
-          </div>
-
-          <div class="element-grupos">
-            <div class="img-container">
-              <img class="img-grupo" src="../images/grupos/Invisible.jpg" />
-            </div>
-            <div class="div-descripcion">
-              <span class="nombre">Invisible</span>
-              <p class="descripcion">
-                Grupo argentino de rock progresivo latinoamericano y jazz
-                fusión, conformado en 1973 por Luis Alberto Spinetta, Héctor
-                “Pomo” Lorenzo y Carlos Alberto “Machi” Rufino.
-              </p>
-            </div>
-          </div>
-
-          <div class="element-grupos">
-            <div class="img-container">
-              <img class="img-grupo" src="../images/grupos/Spinetta-Jade.jpg" />
-            </div>
-            <div class="div-descripcion">
-              <span class="nombre">Spinetta Jade</span>
-              <p class="descripcion">
-                El "proyecto jazzero" integrado por Luis Alberto Spinetta y Pomo
-                Lorenzo, en 1980.
-              </p>
-            </div>
-          </div>
-
-          <div class="element-grupos">
-            <div class="img-container">
-              <img
-                class="img-grupo"
-                src="../images/grupos/Los-Socios-Del-Desierto.jpg"
-              />
-            </div>
-            <div class="div-descripcion">
-              <span class="nombre">Spinetta y Los Socios del Desierto</span>
-              <p class="descripcion">
-                Integrado por Luis Alberto Spinetta, Marcelo Torres y Daniel
-                Wirzt en 1994.
-              </p>
-            </div>
-          </div>
-
-          <div class="element-grupos">
-            <div class="img-container">
-              <img
-                class="img-grupo"
-                src="../images/grupos/Spinetta-Garcia-Ferron.jpg"
-              />
-            </div>
-            <div class="div-descripcion">
-              <span class="nombre">Spinetta, García, Ferrón</span>
-              <p class="descripcion">
-                Proyecto musical formado por Luis Alberto Spinetta, Daniel Ferrón y Rodolfo García en 2011.
-              </p>
-            </div>
-          </div>
-        </div>
+          <?php }
+        } ?>
       </div>
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
